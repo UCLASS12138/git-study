de1cb078c4897bf35367df2f81059b086a964bfd
UCLASS <1794054294.com>
2022-04-03 22:40:41 +0800

    third commit

--- src/main/java/com/uclass/git/gitTest.java
+++ src/main/java/com/uclass/git/gitTest.java
@@ -3,5 +3,7 @@ package com.uclass.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
