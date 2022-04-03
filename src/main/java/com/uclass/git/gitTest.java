d4fdc2026a38289377e96dd08326f416d25e1bf2
UCLASS <1794054294.com>
2022-04-03 22:53:18 +0800

    hot-fix commit1

--- src/main/java/com/uclass/git/gitTest.java
+++ src/main/java/com/uclass/git/gitTest.java
@@ -3,5 +3,8 @@ package com.uclass.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }
